85d7aed6fff94326def57de624c838f33c8f6b12
mohamed_sherif <mohamedshrif1672@gmail.com>
2026-08-29 15:38:02 +0300

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const {PrismaClient} = require('@prisma/client');
-const prisma = new PrismaClient();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(cors())
 
 const PORT = process.env.PORT || 3000;
 
